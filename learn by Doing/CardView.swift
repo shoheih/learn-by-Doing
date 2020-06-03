78605b125f1cc43d818b37ec22795547ea86ea1d
shoheih <shoheigithub@gmail.com>
2020-06-03 10:21:27 +0900

    alert window

--- learn by Doing/CardView.swift
+++ learn by Doing/CardView.swift
@@ -16,6 +16,7 @@ struct CardView: View {
     @State private var fadeIn: Bool = false
     @State private var moveDownward: Bool = false
     @State private var moveUpward: Bool = false
+    @State private var showAlert: Bool = false
     
     var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
     
@@ -42,10 +43,9 @@ struct CardView: View {
             
             Button(action: {
                 print("Button was tapped.")
-                
                 playSound(sound: "sound-chime", type: "mp3")
-                
                 self.hapticImpact.impactOccurred()
+                self.showAlert.toggle()
             }) {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -77,6 +77,12 @@ struct CardView: View {
                 self.moveDownward.toggle()
                 self.moveUpward.toggle()
             }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(card.title),
+                  message: Text(card.message),
+                  dismissButton: .default(Text("OK"))
+            )
         }
     }
 }
